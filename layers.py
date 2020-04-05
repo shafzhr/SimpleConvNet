50e930322c1880b819bacd2035fcb6451439a0d3
shafzhr <shafzhr@gmail.com>
2020-04-05 20:05:20 +0300

    Added basic layer abstract class

--- layers.py
+++ layers.py
@@ -1,7 +1,17 @@
+"""
+Layers
+"""
+from typing import (
+    Tuple,
+)
+import abc
 import numpy as np
 
 
+class Layer(metaclass=abc.ABCMeta):
+    """Layer interface"""
 
-class Conv2D():
-    pass
-
+    @abc.abstractmethod
+    def run(self, x):
+        """feed an input 'x' through the layer"""
+        raise NotImplementedError('users must define "run" to use this base class')
