15024def8670f695395d06f1c14db49acfdae5b3
shafzhr <shafzhr@gmail.com>
2020-04-05 18:44:15 +0300

    Added multiple initializers + comments

--- utils/initializers.py
+++ utils/initializers.py
@@ -13,3 +13,27 @@ def he_normal(shape):
     scale = 2 / fan_in
     stddev = np.sqrt(scale) / .87962566103423978
     return np.random.normal(loc=0, scale=stddev, size=shape)
+
+
+def glorot_uniform(shape):
+    """
+    :param shape: tuple with the shape of the wanted output (filters_amount, depth, height, width)
+    :return: array (it's shape=param shape) with initialized values using 'glorot uniform' initializer
+    """
+    pass
+
+
+def zeros(shape):
+    """
+    :param shape: tuple with the shape of the wanted output (filters_amount, depth, height, width)
+    :return: array (it's shape=param shape) with initialized values using 'zeros' initializer
+    """
+    return np.ones(shape=shape)
+
+
+def ones(shape):
+    """
+    :param shape: tuple with the shape of the wanted output (filters_amount, depth, height, width)
+    :return: array (it's shape=param shape) with initialized values using 'ones' initializer
+    """
+    return np.ones(shape=shape)
