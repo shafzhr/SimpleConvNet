030d69e7622096f1e8dac8aa08ddd78213180b38
shafzhr <shafzhr@gmail.com>
2020-04-08 17:05:05 +0300

    Fixed abstract Layer class

--- CNN/layers/layers.py
+++ CNN/layers/layers.py
@@ -11,28 +11,3 @@ class Layer(metaclass=abc.ABCMeta):
     def run(self, x):
         """feed an input 'x' through the layer"""
         pass
-
-    @abc.abstractmethod
-    def initialize_weights(self, shape):
-        """
-        Initializing weights
-        :param shape: wanted output's shape
-        """
-        pass
-
-    @abc.abstractmethod
-    def initialize_bias(self, shape):
-        """
-        Initializing biases
-        :param shape: wanted output's shape
-        """
-        pass
-
-    @abc.abstractmethod
-    def activation(self, x):
-        """
-        use the layer's activation function over input 'x'
-        :param x: input
-        """
-        pass
-
