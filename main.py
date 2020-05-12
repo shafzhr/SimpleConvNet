a7988f464949298e0d6487e15060d5d46ddfdd23
Shachaf Zohar <shafzhr@gmail.com>
2020-05-12 22:46:58 +0300

    Added model in main

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from CNN.test import unit_test
+from CNN.model import Model
+from CNN.layers.conv import Conv2D
+from CNN.layers.pooling import MaxPooling2D
+from CNN.utils.loss import CategoricalCrossEntropy
+from CNN.layers.core import (
+    Dropout,
+    Flattening,
+    Dense
+)
+from data_preprocess import get_data
 
-TEST = True
+TEST = False
 
 
 def test():
@@ -8,7 +18,23 @@ def test():
 
 
 def main():
-    pass
+    model = Model()
+    model.add(Conv2D(32, (3, 3), 'relu', 'he_normal', 'zeros', 1))
+    model.add(MaxPooling2D((2, 2), 2))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(64, (3, 3), 'relu', 'glorot_uniform', 'zeros', 1))
+    model.add(MaxPooling2D((2, 2), 2))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(128, (3, 3), 'relu', 'glorot_uniform', 'zeros', 1))
+    model.add(Dropout(0.4))
+    model.add(Flattening())
+    model.add(Dense(128, 'relu', 'glorot_uniform', 'zeros'))
+    model.add(Dropout(0.3))
+    model.add(Dense(2, 'softmax', 'glorot_uniform', 'zeros'))
+    model.set_loss(CategoricalCrossEntropy)
+
+    data = get_data()
+    model.train(data, 32, 100, 'adam', lr=1e-4, beta1=0.9, beta2=0.999, epsilon=1e-7)
 
 
 if __name__ == '__main__':
